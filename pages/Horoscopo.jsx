f69bd3baaf302b26bbf6e2e9e300746776fde261
mylifeasggy <ginellysmsuarez@gmail.com>
2025-07-10 10:16:04 -0400

    react route dom

---
+++ pages/Horoscopo.jsx
@@ -0,0 +1,15 @@
+
+import { Link } from "react-router-dom";
+
+const Horoscopo = () => {
+    return (
+
+        <div>
+            <h2>Working on int </h2>
+            <Link to="/"> Daily Tarot</Link>
+        </div>
+       
+    );
+}
+
+export default Horoscopo;
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import Header from "../components/Header"
 import MainBoard from "../components/MainBoard"
+import { Route, Routes } from "react-router-dom"
+import Horoscopo from "../pages/Horoscopo"
 
 
 
 function App() {
 
   return (
-   <>
-    <Header/>
-    <MainBoard/>
-
-    </>
+    <Routes>
+      <Header/>
+      <Route path="/ " element={MainBoard} />
+      <Route path="/Horoscopo" element={Horoscopo} />
+    </Routes>
 
   )
 }
