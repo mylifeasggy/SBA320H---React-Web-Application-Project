1b52ade0205276fe5f52859848b1ff12b0f6fd84
mylifeasggy <ginellysmsuarez@gmail.com>
2025-07-10 07:41:50 -0400

    not liking the display

--- components/CardDetail.jsx
+++ components/CardDetail.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
 
-const CardDetail = () => {
+const CardDetail = ({card}) => {
+if(!card) {
+    return <p>Loading card</p>
+}
+
+console.log("🔮 CardDetail received:", card);
     return (
-        <div>
+        <div className="container">
+            <h1 className="">{card.card}({card.category})</h1>
+            <div className="card-container">
+                <img className="card-image" src={card.image} alt={card.card} />
+                <div className="sections">
+                    <p><strong>❤️ Love:</strong> {card.love}</p>
+                    <p><strong>💼 Career:</strong> {card.career}</p>
+                    <p><strong>💰 Finance:</strong> {card.finance}</p>
+                </div>
+            </div>
             
         </div>
     );
